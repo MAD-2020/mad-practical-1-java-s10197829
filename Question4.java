b8aa833f768a03cdb0c33bf3b1c5d70770b91bbf
s10197829 <60372776+s10197829@users.noreply.github.com>
2020-04-26 16:52:44 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -4,6 +4,7 @@ public class Question4
 {
   public static void main(String[] args)
   {
+   
     /**
      * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
      *    e.g.
@@ -17,7 +18,13 @@ public class Question4
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
      
-    Scanner in = new Scanner(System.in);
-    
+    Scanner input = new Scanner(System.in);
+    int num = input.nextInt();
+    for (int i = num; i > 0; i --){
+      for (int j = 0; j < i; j ++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
